3bcdfbfe58cd2b69472996d80dbabf708ca6e98c
DevEngineer <devopgit@gmail.com>
2023-02-25 22:39:22 -0700

    modified config file

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
-customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
-customuser = "intellipaat"
-custompass = "intellipaat123"
+# customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+# customuser = "intellipaat"
+# custompass = "intellipaat123"
+# customdb = "employee"
+# custombucket = "addemployee"
+# customregion = "us-east-2"
+
+
+#hostname or endpoint url of AWS RDS
+customhost = "employeedb.ck9zemzq2vpd.us-west-2.rds.amazonaws.com"
+
+#username and pwd
+customuser = "admin"
+custompass = "marceladmin"
+
+#database
 customdb = "employee"
-custombucket = "addemployee"
+#bucket name
+custombucket = "primusterra-app"
+
+#AWS region where located the bucket
 customregion = "us-east-2"
